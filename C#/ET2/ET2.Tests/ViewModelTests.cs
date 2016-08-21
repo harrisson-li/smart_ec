08127df9021c3dde1f67e2feb4687f92e73b42d5
Toby Qin <toby.qin@ef.com>
2016-08-21 21:20:33 +0800

    Able to resolve test account and remember recent test accounts

--- C#/ET2/ET2.Tests/ViewModelTests.cs
+++ C#/ET2/ET2.Tests/ViewModelTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using EF.Common;
 using ET2.Models;
 using ET2.Support;
+using ET2.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ET2.Tests
@@ -10,5 +11,41 @@ namespace ET2.Tests
     [TestClass]
     public class ViewModelTests : TestBase
     {
+        public ShellViewModel Shell { get; set; }
+
+        public ViewModelTests()
+        {
+            ShellViewModel.InitForTest();
+            Shell = ShellViewModel.Instance;
+        }
+
+        [TestMethod]
+        public void TestGetAccountInfoByName()
+        {
+            var name = "ctest2515";
+            Shell.TestEnvVM.UpdateEnvironment("qa");
+            var account = Shell.TestAccountVM.GetTestAccountByNameOrId(name);
+            Assert.AreEqual(name, account.UserName);
+            Assert.AreEqual("1", account.Password);
+        }
+
+        [TestMethod]
+        public void TestGetAccountInfoById()
+        {
+            var id = "10777008";
+            Shell.TestEnvVM.UpdateEnvironment("qa");
+            var account = Shell.TestAccountVM.GetTestAccountByNameOrId(id);
+            Assert.AreEqual(id, account.MemberId);
+            Assert.AreEqual("1", account.Password);
+        }
+
+        [TestMethod]
+        public void TestGetAccountInfoByInvalid()
+        {
+            var id = "invalid";
+            Shell.TestEnvVM.UpdateEnvironment("qa");
+            var account = Shell.TestAccountVM.GetTestAccountByNameOrId(id);
+            Assert.AreEqual(null, account);
+        }
     }
 }
--- C#/ET2/ET2/ReleaseNote.md
+++ C#/ET2/ET2/ReleaseNote.md
@@ -1,4 +1,9 @@
-﻿## v0.0.0.19 - Preview
+﻿## v0.0.0.21 - Preview
+
+1. Able to resolve test account when input name or member id of student
+2. Remember recent 20 generated test accounts, you can select from user name dropdown list.
+
+## v0.0.0.19 - Preview
 
 1. Support test account type: E10, S15 and S15 V2.0
 2. Able to activate E10 test account.
