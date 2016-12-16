32c137a787bedad5e1c8223774a7a6b6fead48de
Toby Qin <toby.qin@ef.com>
2016-12-16 13:51:22 +0800

    update ectools.

--- python/ectools/build.py
+++ python/ectools/build.py
@@ -101,7 +101,7 @@ def upload_package():
 
 def make_doc():
     print('Make document')
-    cmd = 'cmd / c """{}"" html"'.format(doc_cmd)
+    cmd = 'cmd / c "{}" html'.format(doc_cmd)
     print(cmd)
     os.system(cmd)
 
