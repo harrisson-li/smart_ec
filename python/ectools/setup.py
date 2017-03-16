be872fabd258baaee9c7663117f88b61b3118750
Toby Qin <toby.qin@live.com>
2017-03-16 16:37:31 +0800

    update version.

--- python/ectools/setup.py
+++ python/ectools/setup.py
@@ -6,7 +6,7 @@ def main():
         name="ectools",
         description="Toolbox from EFEC QA team.",
         long_description="library to help with EFEC testing, such as create test account, submit score, get tokens.",
-        version="1.3.0",
+        version="1.3.2",
         author="Toby Qin",
         author_email="toby.qin@ef.com",
         url="https://confluence.englishtown.com/display/SMart/ectools+-+Introduction",
