864a6093d163071b462fb8607fdbe649d85888e3
Toby Qin <toby.qin@live.com>
2017-06-05 17:29:24 +0800

    fix unit tests.

--- python/ectools/setup.py
+++ python/ectools/setup.py
@@ -6,7 +6,7 @@ def main():
         name="ectools",
         description="Toolbox from EFEC QA team.",
         long_description="library to help with EFEC testing, such as create test account, submit score, get tokens.",
-        version="1.3.48",
+        version="1.3.50",
         author="Toby Qin",
         author_email="toby.qin@ef.com",
         url="https://confluence.englishtown.com/display/SMart/ectools+-+Introduction",
