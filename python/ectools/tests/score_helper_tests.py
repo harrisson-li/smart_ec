d4aee5b3ed898351a7153eb8a0f37186919d1379
Joshua Zhu <joshuazhu1986@outlook.com>
2019-03-26 18:34:08 +0800

    add verify score for submit lesson

--- python/ectools/tests/score_helper_tests.py
+++ python/ectools/tests/score_helper_tests.py
@@ -15,3 +15,15 @@ def test_submit_score_helper_v2():
         pass_six_units_and_level_test()
 
     achieve_minimum_class_taken(student_id=student_id)
+
+
+def test_submit_lesson_score():
+    set_environment('staging')
+    student = activate_school_v2_student()
+    student_id = student['member_id']
+    get_logger().info(student)
+
+    with submit_score_helper(student_id=student_id):
+        submit_merged_lesson_for_current_unit(1, 100)
+        submit_pc_lesson_for_current_unit(2, 100)
+        submit_mobile_lesson_for_current_unit(3, 100)
