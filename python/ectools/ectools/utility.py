8431d5229840ddfa824e655b0f2d3d817eb9c66e
blair.liu <Blair.Liu@ef.com>
2020-01-20 11:43:16 +0800

    keep utility unchanged

--- python/ectools/ectools/utility.py
+++ python/ectools/ectools/utility.py
@@ -505,4 +505,3 @@ def config_sys_logging(to_console=True, log_file_dir=None, log_file_name=None):
 def password_generator(password_length=8):
     letters_digits = string.ascii_lowercase + string.ascii_uppercase + string.digits
     return ''.join(random.sample(letters_digits, password_length))
-
