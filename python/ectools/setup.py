362051ea1831c5a325070cfe785231e5eab123dc
blair <blair.liu@ef.com>
2020-04-21 18:22:11 +0800

    update version

--- python/ectools/setup.py
+++ python/ectools/setup.py
@@ -6,7 +6,7 @@ def main():
         name="ectools",
         description="Toolbox from EFEC QA team.",
         long_description="Library to help with EFEC testing, such as create test account, submit score, get tokens.",
-        version="1.8.17",
+        version="1.8.19",
         author="Toby Qin",
         author_email="toby.qin@ef.com",
         url="https://confluence.eflabs.cn/display/SMart/ectools+-+Introduction",
